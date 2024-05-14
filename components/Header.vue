30b654ef8ec0df5438186dc3a90badb7e3b7533f
Suhaib Janjua <suhaib.janjua@gmail.com>
2024-05-15 01:45:12 +0500

    updated website content

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
         <Slide class="lg:hidden">
             <nuxt-link to="/" class="mr-4">Home</nuxt-link>
             <nuxt-link to="about" class="mr-4">About Me</nuxt-link>
-            <a target="_blank" href="https://blog.luckyadogun.com" class="mr-4">Blog <small class="ml-1 flex items-center text-xs">- FOR DEVS</small></a>
+            <a target="_blank" href="https://suhaibjanjua.github.io" class="mr-4">Blog <small class="ml-1 flex items-center text-xs">- FOR DEVS</small></a>
             <nuxt-link to="toolbox" class="mr-4">ToolBox <small class="ml-1 flex items-center text-xs">- FOR DEVS</small></nuxt-link>
             <!-- <nuxt-link to="/" class="flex mr-4 tracking-tighter">SaaS<strong>CAMP</strong> <small class="ml-1 text-xs tracking-tighter leading-tight">- FOR FOUNDERS</small></nuxt-link> -->
         </Slide>        
@@ -11,7 +11,7 @@
         <div id="web-header" class="flex">
             <div class="hidden lg:flex">
                 <nuxt-link to="about" class="mr-4">About Me</nuxt-link>
-                <a target="_blank" href="https://blog.luckyadogun.com" class="mr-4">Blog</a>
+                <a target="_blank" href="https://suhaibjanjua.github.io" class="mr-4">Blog</a>
                 <nuxt-link to="toolbox" class="mr-4">ToolBox</nuxt-link>                    
                 <!-- <nuxt-link to="/" class="flex mr-4 tracking-tighter">SaaS<strong>CAMP</strong><small class="text-xs tracking-tighter leading-tight"> - for Founders</small></nuxt-link> -->
             </div>
